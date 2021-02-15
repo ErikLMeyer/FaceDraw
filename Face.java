c96b150562d227e69a667aa9d92c6c6457623271
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-15 14:25:08 -0600

    initial commit

---
+++ Face.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Face extends JPanel {
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        
+    }
+}
