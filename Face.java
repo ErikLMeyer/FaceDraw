b88ccdabe4d7a990cfc8a04f4d68b310f9f2a5bd
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-15 15:19:00 -0600

    Added constants, variable, and started constructor

--- Face.java
+++ Face.java
@@ -2,9 +2,28 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class Face extends JPanel {
+    static final int FROWN[] = {45, 90};
+    static final int NEUTRAL[] = {0, 180};
+    static final int SMILE[] = {-45, -90};
+
+    private Oval head;
+    private Oval eyes[] = new Oval[2];
+    private Smile smile;
+
+    Face(){
+        this(0, 0, 0, 0);
+    }
+
+    Face(int x, int y, int width, int height){
+        head.setX(x);
+        head.setY(y);
+        head.setWidth(width);
+        head.setHeight(height);
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        
+
     }
 }
