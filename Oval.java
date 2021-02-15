7dda0089fe985dc88eac20c5dee6eb359ced683e
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-15 14:25:56 -0600

    Initial commit. Getters, setters, and constructors

---
+++ Oval.java
@@ -0,0 +1,50 @@
+public class Oval {
+    private int widthRadius, heightRadius;
+    private int xPosition, yPosition;
+
+    public void setWidth(int w){
+        widthRadius = w;
+    }
+
+    public void setHeight(int h){
+        heightRadius = h;
+    }
+
+    public void setX(int x){
+        xPosition = x;
+    }
+
+    public void setY(int y){
+        yPosition = y;
+    }
+
+    public int getWidth(){
+        return widthRadius;
+    }
+
+    public int getHeight(){
+        return heightRadius;
+    }
+
+    public int getX(){
+        return xPosition;
+    }
+
+    public int getY(){
+        return yPosition;
+    }
+
+    Oval(){
+        setWidth(0);
+        setHeight(0);
+        setX(0);
+        setY(0);
+    }
+
+    Oval(int width, int height, int x, int y){
+        setWidth(width);
+        setHeight(height);
+        setX(x);
+        setY(y);
+    }
+}
